45f517f5701ba5921744ebe912010c29333e77b1
qJeffTheKiller <muha_andrei@yahoo.ro>
2020-08-31 15:18:34 +0100

    yet another hotfix, also reverting the debug, did it the other way arround

--- blog/models.py
+++ blog/models.py
@@ -31,5 +31,5 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
 
-def approved_comments(self):
-    return self.comments.filter(approved_comment=True)
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(fbp7d5e_+^v&p@m%ztzcn0z0$f**pf-fk)w*mxw&j$s)lamy$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com', 'qjeffthekiller.pythonanywhere.com']
 
